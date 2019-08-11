d49d5a7117b2907860b53aba70b1622938a10ca8
EA Rashel <rashel320@gmail.com>
2019-08-11 23:33:26 +0600

    my first junit test for positive number

---
+++ app/src/main/java/com/work/androidtestingandtdd/PositiveNumberValidator.java
@@ -0,0 +1,8 @@
+package com.work.androidtestingandtdd;
+
+public class PositiveNumberValidator {
+
+    public boolean isPositive(int number) {
+        return number > 0;
+    }
+}
---
+++ app/src/test/java/com/work/androidtestingandtdd/PositiveNumberValidatorTest.java
@@ -0,0 +1,40 @@
+package com.work.androidtestingandtdd;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class PositiveNumberValidatorTest {
+
+    PositiveNumberValidator SUT; // System Under Test
+
+    @Before
+    public void setup() {
+        SUT = new PositiveNumberValidator();
+
+    }
+
+
+    @Test
+    public void test1() {
+        boolean result = SUT.isPositive(-1);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void test2() {
+        boolean result = SUT.isPositive(0);
+        Assert.assertThat(result, is(false));
+    }
+
+
+    @Test
+    public void test3() {
+        boolean result = SUT.isPositive(1);
+        assertThat(result, is(true));
+    }
+
+}
