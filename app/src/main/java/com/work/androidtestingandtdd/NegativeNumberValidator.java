996d4c22d359a9877149fb09570436523ce5010e
EA Rashel <rashel320@gmail.com>
2019-08-12 00:05:11 +0600

    complete exercise 1

---
+++ app/src/main/java/com/work/androidtestingandtdd/NegativeNumberValidator.java
@@ -0,0 +1,11 @@
+package com.work.androidtestingandtdd;
+
+public class NegativeNumberValidator {
+
+    public boolean isNegative(int number) {
+        // the bug is that 0 will be reported as negative while it's not
+//        return number <= 0;
+        // fixed
+        return number < 0;
+    }
+}
